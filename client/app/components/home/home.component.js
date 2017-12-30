c39015a04cf2e089e2aa96b0aea54d39db19721f
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-31 00:41:49 +0200

    refactor

--- client/app/components/home/home.component.js
+++ client/app/components/home/home.component.js
@@ -1,59 +1,61 @@
-(function() {
-    'use strict';
+(function () {
+  // Usage:
+  //
+  // Creates:
+  //
 
-    // Usage:
-    //
-    // Creates:
-    //
+  angular.module('lanser').component('home', {
+    templateUrl: '/components/home/home.html',
+    controller: ControllerController,
+    controllerAs: 'vm'
+  });
 
-    angular.module('lanser').component('home', {
-        templateUrl: '/components/home/home.html',
-        controller: ControllerController,
-        controllerAs: 'vm'
-    });
+  ControllerController.$inject = ['jobService', 'localStorageService', '$scope'];
 
-    ControllerController.$inject = ['jobService', 'localStorageService', '$scope'];
-    function ControllerController(jobService, localStorageService, $scope) {
-        var vm = this;
-        vm.searchSkill=[];
-        vm.clicked = '';
-        activate();
-        vm.currentPage = 0;
-        vm.pageSize = 10;
-        vm.applyToJob = applyToJob;
-        vm.change = change;
-        vm.changeListToMap = changeListToMap;
-        vm.listOrMap = 'Show Map';
+  function ControllerController(jobService, localStorageService, $scope) {
+    const vm = this;
+    vm.searchSkill = [];
+    vm.clicked = '';
+    activate();
+    vm.currentPage = 0;
+    vm.pageSize = 10;
+    vm.applyToJob = applyToJob;
+    vm.change = change;
+    vm.changeListToMap = changeListToMap;
+    vm.listOrMap = 'Show Map';
 
-        ////////////////
-        function changeListToMap() {
-            vm.showListOrMap = !vm.showListOrMap;
-            vm.listOrMap === 'Show Map' ? (vm.listOrMap = 'Show List') : (vm.listOrMap = 'Show On Map');
-        }
-
-        function change() {
-            vm.jobDescription = !vm.jobDescription;
-        }
-
-        function activate() {
-            jobService.getJobs().then(function() {
-                vm.jobs = jobService.jobs;
-                vm.titles = Object.keys(vm.jobs[0], 1);
-            });
-        }
-
-        function applyToJob(job_id) {
-            let user = localStorageService.get('userId');
-            if (!user) {
-                $log.log('no user');
-                return $state.go('login');
-            }
-
-            jobService.applyToJob(job_id, user.id).then(data => {
-                let message = ['Thank you For Applying For The Job', 'You Have allReady Applied To the Job'];
-                console.log(data.data.nModified);
-                data.data.nModified ? (vm.applyMessage = message[0]) : (vm.applyMessage = message[1]);
-            });
-        }
+    // //////////////
+    function changeListToMap() {
+      vm.showListOrMap = !vm.showListOrMap;
+      vm.listOrMap === 'Show Map' ? (vm.listOrMap = 'Show List') : (vm.listOrMap = 'Show On Map');
     }
-})();
+
+    function change() {
+      vm.jobDescription = !vm.jobDescription;
+    }
+
+    function activate() {
+      jobService.getJobs().then(() => {
+        vm.jobs = jobService.jobs;
+        vm.titles = Object.keys(vm.jobs[0], 1);
+      });
+    }
+
+    function applyToJob(job_id) {
+      const user = localStorageService.get('userId');
+      if (!user) {
+        $log.log('no user');
+        return $state.go('login');
+      }
+
+      jobService.applyToJob(job_id, user.id).then(data => {
+        const message = [
+          'Thank you For Applying For The Job',
+          'You Have allReady Applied To the Job'
+        ];
+        console.log(data.data.nModified);
+        data.data.nModified ? (vm.applyMessage = message[0]) : (vm.applyMessage = message[1]);
+      });
+    }
+  }
+}());
--- client/app/components/job/job.component.js
+++ client/app/components/job/job.component.js
@@ -1,34 +1,32 @@
-(function() {
-    'use strict';
+(function () {
+  // Usage:
+  //
+  // Creates:
+  //
 
-    // Usage:
-    //
-    // Creates:
-    //
-
-    angular.module('lanser').component('job', {
-        templateUrl: '/components/job/job.html',
-        controller: jobController,
-        controllerAs: 'vm',
-        bindings: {
-            job: '<',
-            buttonName: '<',
-            clickFunction:'&',
-            jobUpdate: '<',
-            publishName:'<'
-        }
-    });
-
-    // jobController.$inject = ['jobService'];
-    function jobController() {
-        var vm = this;
-        vm.buttonClicked = false;
-        vm.description = false;
-        vm.showDescription = showDescription;
-        ////////////////
-
-        function showDescription() {
-            vm.description = !vm.description;
-        }
+  angular.module('lanser').component('job', {
+    templateUrl: '/components/job/job.html',
+    controller: jobController,
+    controllerAs: 'vm',
+    bindings: {
+      job: '<',
+      buttonName: '<',
+      clickFunction: '&',
+      jobUpdate: '<',
+      publishName: '<'
     }
-})();
+  });
+
+  // jobController.$inject = ['jobService'];
+  function jobController() {
+    const vm = this;
+    vm.buttonClicked = false;
+    vm.description = false;
+    vm.showDescription = showDescription;
+    // //////////////
+
+    function showDescription() {
+      vm.description = !vm.description;
+    }
+  }
+}());
