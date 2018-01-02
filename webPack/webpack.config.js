ac9d2ab6b9dabefa1c4875a4350d81e0fb23e779
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-03 01:57:27 +0200

    finished intalling webpack

--- webPack/webpack.config.js
+++ webPack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,8 +11,26 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css/, use: ['style-loader', 'css-loader'] }
+      {
+        test: /\.css/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin('globals.css'),
+    new CopyWebpackPlugin([{ from: 'src/index.html' }])
+  ],
+
+  devServer: {
+    contentBase: './',
+    compress: true,
+    port: 3000,
+    stats: 'minimal',
+    open: true
   },
   devtool: 'source-map'
 };
