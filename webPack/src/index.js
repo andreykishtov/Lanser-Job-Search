4df7d7f6d0e5165c0a27cacb58953650b6edc4f0
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-03 01:42:31 +0200

    css webpack config

--- webPack/src/index.js
+++ webPack/src/index.js
@@ -1,3 +1,12 @@
 import greetings from './robot';
 
 document.write(greetings('Affirmative', 'Dave'));
+
+import styles from './globals.css';
+
+const element = `
+<div class="element">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</p>
+</div>
+`;
+document.write(element);
--- webPack/webpack.config.js
+++ webPack/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     filename: 'app.bundle.js'
   },
   module: {
-    rules: [{ test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }]
-  }
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  devtool: 'source-map'
 };
